4b5c5ece88d748f4f5088054c2d0f982de7e6ecd
kim__yun_h <ajsl9749@naver.com>
2026-05-12 18:13:33 +0900

    feat(IN-278): 북마크 페이지 필터 헤더 수정
    /influencer/bookmarked 페이지 생성
    /influencer/bookmarked여부에 따라 src\widgets\influencer\ui\InfluencerFilter.tsx의 랜더링이 바뀌도록 수정

--- src/widgets/influencer/ui/InfluencerFilter.tsx
+++ src/widgets/influencer/ui/InfluencerFilter.tsx
@@ -6,6 +6,7 @@ import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 import { SearchBar } from '@/shared/ui/search-bar'
 import { Button } from '@/shared/ui/button'
 import IconHeart from '@/shared/assets/heart-bold.svg?react'
+import IconLeftArrow from '@/shared/assets/leftwards-arrow-bold.svg?react'
 import {
   DropdownTrigger,
   CategoryNamesDropdown,
@@ -199,163 +200,187 @@ function InfluencerFilterInner() {
     return () => clearTimeout(timer)
   }, [query, isFocused, applyChannelNameToUrl])
 
+  const isBookmarkPage = pathname === '/influencer/bookmarked'
+
   return (
-    <div className='flex h-fit w-full items-center gap-24 bg-background-gray-default p-24'>
-      {/* 검색바 */}
-      <SearchBar
-        placeholder='채널명 또는 키워드 검색'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onClear={() => {
-          setQuery('')
-          applyChannelNameToUrl('')
-        }}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            applyChannelNameToUrl(query)
-            e.currentTarget.blur()
-          }
-        }}
-      />
+    <div className='flex h-fit w-full flex-col items-center gap-16 bg-background-gray-default p-24'>
+      {/** 보관함 페이지 헤더
+       * /influencer/bookmarked일 때만 랜더링
+       */}
+      {isBookmarkPage && (
+        <div className='flex h-[6.8rem] w-full gap-16 pt-24 pr-24 pb-16 pl-24'>
+          <button
+            type='button'
+            onClick={() => router.push('/influencer')}
+            className='flex size-24 shrink-0 items-center gap-10'>
+            <IconLeftArrow className='size-full' />
+          </button>
+          <span className='size-fit w-full text-ibm-title-lg-normal text-text-and-icon-default'>
+            보관함
+          </span>
+        </div>
+      )}
 
-      {/* 필터 */}
-      <div className='flex h-fit w-full flex-1 items-center gap-12'>
-        <DropdownTrigger
-          label='카테고리'
-          output={category.output}
-          outputQuery={category.outputQuery}>
-          {(onClose) => (
-            <CategoryNamesDropdown
-              defaultValue={
-                category.outputQuery ? category.outputQuery.split(',') : []
-              }
-              onChange={(output, outputQuery) => {
-                setCategory({ output, outputQuery })
-                applyCategoriesToUrl(outputQuery)
-                onClose()
-              }}
-            />
-          )}
-        </DropdownTrigger>
-
-        <DropdownTrigger
-          label='구독자 수'
-          output={subscriber.output}
-          outputQuery={subscriber.outputQuery}>
-          {(onClose) => {
-            const [from, to] = subscriber.outputQuery
-              ? subscriber.outputQuery.split(',')
-              : ['', '']
-            return (
-              <SubscriberDropdown
-                defaultFrom={from ?? ''}
-                defaultTo={to ?? ''}
-                onChange={(output, outputQuery) => {
-                  setSubscriber({ output, outputQuery })
-                  applySubscriberToUrl(outputQuery)
-                  onClose()
-                }}
-              />
-            )
+      <div className='flex w-full items-center gap-24'>
+        {/* 검색바 */}
+        <SearchBar
+          placeholder='채널명 또는 키워드 검색'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onClear={() => {
+            setQuery('')
+            applyChannelNameToUrl('')
           }}
-        </DropdownTrigger>
-
-        <DropdownTrigger
-          label='업로드 주기'
-          output={uploadPeriod.output}
-          outputQuery={uploadPeriod.outputQuery}>
-          {(onClose) => (
-            <UploadPeriodDropdown
-              defaultValue={
-                uploadPeriod.outputQuery
-                  ? uploadPeriod.outputQuery.split(',')
-                  : []
-              }
-              onChange={(output, outputQuery) => {
-                setUploadPeriod({ output, outputQuery })
-                applyUploadPeriodToUrl(outputQuery)
-                onClose()
-              }}
-            />
-          )}
-        </DropdownTrigger>
-
-        <DropdownTrigger
-          label='광고 이력'
-          output={hasAdHistory.output}
-          outputQuery={hasAdHistory.outputQuery}>
-          {(onClose) => (
-            <HasAdHistoryDropdown
-              defaultValue={hasAdHistory.outputQuery}
-              onChange={(output, outputQuery) => {
-                setHasAdHistory({ output, outputQuery })
-                applyHasAdHistoryToUrl(outputQuery)
-                onClose()
-              }}
-            />
-          )}
-        </DropdownTrigger>
-
-        <DropdownTrigger
-          label='참여율'
-          output={engagementRate.output}
-          outputQuery={engagementRate.outputQuery}>
-          {(onClose) => {
-            const [from, to] = engagementRate.outputQuery
-              ? engagementRate.outputQuery.split(',')
-              : ['', '']
-
-            return (
-              <EngagementRateDropdown
-                defaultFrom={from ?? ''}
-                defaultTo={to ?? ''}
-                onChange={(output, outputQuery) => {
-                  setEngagementRate({ output, outputQuery })
-                  applyEngagementRateToUrl(outputQuery)
-                  onClose()
-                }}
-              />
-            )
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              applyChannelNameToUrl(query)
+              e.currentTarget.blur()
+            }
           }}
-        </DropdownTrigger>
-
-        <DropdownTrigger
-          label='Outlier 배수'
-          output={outlierRange.output}
-          outputQuery={outlierRange.outputQuery}>
-          {(onClose) => (
-            <OutlierRangeDropdown
-              defaultValue={outlierRange.outputQuery}
-              onChange={(output, outputQuery) => {
-                setOutlierRange({ output, outputQuery })
-                applyOutlierRangeToUrl(outputQuery)
-                onClose()
-              }}
-            />
-          )}
-        </DropdownTrigger>
-
-        {/* TODO: 기획단에서 언어와 관련된 필터값 논의중 */}
-        <DropdownTrigger
-          label='언어'
-          output={language.output}
-          outputQuery={language.outputQuery}
-          onChange={(output, outputQuery) =>
-            setLanguage({ output, outputQuery })
-          }
         />
-      </div>
 
-      {/* 보관함 버튼 */}
-      <Button
-        color='primary'
-        variant='outlined'
-        size='sm'
-        rightIcon={<IconHeart />}>
-        보관함
-      </Button>
+        {/* 필터 */}
+        <div className='flex h-fit w-full flex-1 items-center gap-12'>
+          <DropdownTrigger
+            label='카테고리'
+            output={category.output}
+            outputQuery={category.outputQuery}>
+            {(onClose) => (
+              <CategoryNamesDropdown
+                defaultValue={
+                  category.outputQuery ? category.outputQuery.split(',') : []
+                }
+                onChange={(output, outputQuery) => {
+                  setCategory({ output, outputQuery })
+                  applyCategoriesToUrl(outputQuery)
+                  onClose()
+                }}
+              />
+            )}
+          </DropdownTrigger>
+
+          <DropdownTrigger
+            label='구독자 수'
+            output={subscriber.output}
+            outputQuery={subscriber.outputQuery}>
+            {(onClose) => {
+              const [from, to] = subscriber.outputQuery
+                ? subscriber.outputQuery.split(',')
+                : ['', '']
+              return (
+                <SubscriberDropdown
+                  defaultFrom={from ?? ''}
+                  defaultTo={to ?? ''}
+                  onChange={(output, outputQuery) => {
+                    setSubscriber({ output, outputQuery })
+                    applySubscriberToUrl(outputQuery)
+                    onClose()
+                  }}
+                />
+              )
+            }}
+          </DropdownTrigger>
+
+          <DropdownTrigger
+            label='업로드 주기'
+            output={uploadPeriod.output}
+            outputQuery={uploadPeriod.outputQuery}>
+            {(onClose) => (
+              <UploadPeriodDropdown
+                defaultValue={
+                  uploadPeriod.outputQuery
+                    ? uploadPeriod.outputQuery.split(',')
+                    : []
+                }
+                onChange={(output, outputQuery) => {
+                  setUploadPeriod({ output, outputQuery })
+                  applyUploadPeriodToUrl(outputQuery)
+                  onClose()
+                }}
+              />
+            )}
+          </DropdownTrigger>
+
+          <DropdownTrigger
+            label='광고 이력'
+            output={hasAdHistory.output}
+            outputQuery={hasAdHistory.outputQuery}>
+            {(onClose) => (
+              <HasAdHistoryDropdown
+                defaultValue={hasAdHistory.outputQuery}
+                onChange={(output, outputQuery) => {
+                  setHasAdHistory({ output, outputQuery })
+                  applyHasAdHistoryToUrl(outputQuery)
+                  onClose()
+                }}
+              />
+            )}
+          </DropdownTrigger>
+
+          <DropdownTrigger
+            label='참여율'
+            output={engagementRate.output}
+            outputQuery={engagementRate.outputQuery}>
+            {(onClose) => {
+              const [from, to] = engagementRate.outputQuery
+                ? engagementRate.outputQuery.split(',')
+                : ['', '']
+
+              return (
+                <EngagementRateDropdown
+                  defaultFrom={from ?? ''}
+                  defaultTo={to ?? ''}
+                  onChange={(output, outputQuery) => {
+                    setEngagementRate({ output, outputQuery })
+                    applyEngagementRateToUrl(outputQuery)
+                    onClose()
+                  }}
+                />
+              )
+            }}
+          </DropdownTrigger>
+
+          <DropdownTrigger
+            label='Outlier 배수'
+            output={outlierRange.output}
+            outputQuery={outlierRange.outputQuery}>
+            {(onClose) => (
+              <OutlierRangeDropdown
+                defaultValue={outlierRange.outputQuery}
+                onChange={(output, outputQuery) => {
+                  setOutlierRange({ output, outputQuery })
+                  applyOutlierRangeToUrl(outputQuery)
+                  onClose()
+                }}
+              />
+            )}
+          </DropdownTrigger>
+
+          {/* TODO: 기획단에서 언어와 관련된 필터값 논의중 */}
+          <DropdownTrigger
+            label='언어'
+            output={language.output}
+            outputQuery={language.outputQuery}
+            onChange={(output, outputQuery) =>
+              setLanguage({ output, outputQuery })
+            }
+          />
+        </div>
+
+        {/* 보관함 버튼: /influencer 페이지에서만 노출 */}
+        {!isBookmarkPage && (
+          <Button
+            color='primary'
+            variant='outlined'
+            size='sm'
+            rightIcon={<IconHeart />}
+            onClick={() => router.push('/influencer/bookmarked')}>
+            보관함
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
