618acc3d1f26ae500502f846e44ae0854ec78391
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2026-05-14 22:57:55 +0900

    fix: api path 수정

--- src/features/auth/api/channelApi.ts
+++ src/features/auth/api/channelApi.ts
@@ -21,7 +21,7 @@ export async function refreshChannel(
 
 export async function fetchConnectedChannelProfile(): Promise<ChannelProfileDto> {
   const res = await axiosInstance.get<ApiResponse<ChannelProfileDto>>(
-    'user/channels/main'
+    '/user/channels/main'
   )
   return res.data.responseDto
 }
