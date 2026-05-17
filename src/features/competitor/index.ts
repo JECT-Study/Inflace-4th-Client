7c68f3cad61c38b64a70dd6b52ed5b2e8919c9f0
Seonghoon Cho <86822777+shunn2@users.noreply.github.com>
2026-05-17 22:34:56 +0900

    fix: 리뷰 피드백 반영

--- src/features/competitor/index.ts
+++ src/features/competitor/index.ts
@@ -4,7 +4,6 @@ export type {
   SortCriteria,
   SortOrder,
   BrandCollaborationDto,
-  BrandCollaborationsPageInfo,
   BrandCollaborationsSort,
   BrandCollaborationsResponseDto,
   BrandCollaborationsQuery,
