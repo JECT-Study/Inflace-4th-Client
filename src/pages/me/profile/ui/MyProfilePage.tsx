5fcc068a1e270bcffd37104a024671d1c9220dd5
kim__yun_h <ajsl9749@naver.com>
2026-05-19 01:36:52 +0900

    Update MyProfilePage.tsx

--- src/pages/me/profile/ui/MyProfilePage.tsx
+++ src/pages/me/profile/ui/MyProfilePage.tsx
@@ -6,7 +6,6 @@ import { CustomInfoSection } from '@/widgets/me/customInfo'
 export function MyProfilePage() {
   return (
     <div className='flex size-full bg-background-gray-default'>
-      <MyPageSidebar currentPath='/me/profile' />
       <div className='flex h-fit w-full flex-col gap-32 p-24'>
         <h1 className='text-ibm-title-lg-normal text-text-and-icon-default'>
           프로필 설정
